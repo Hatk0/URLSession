02b8240832e697f74be851940daabff20217176d
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-20 21:19:50 +0300

    Error number output

--- URLSession.playground/Contents.swift
+++ URLSession.playground/Contents.swift
@@ -5,7 +5,7 @@ func getData(urlRequest: String) {
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
-            
+            print("Ошибка: \(error)")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
