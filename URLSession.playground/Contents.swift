0f1c0b7033164e0aeedaac75109555f6b7a666d9
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-22 20:30:35 +0300

    Create md5 and generateHash to generate hash

--- URLSession.playground/Contents.swift
+++ URLSession.playground/Contents.swift
@@ -1,4 +1,29 @@
 import Foundation
+import CryptoKit
+
+func MD5(string: String) -> Data {
+    let messageData = string.data(using:.utf8)!
+    var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+
+    _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+        messageData.withUnsafeBytes { messageBytes -> UInt8 in
+            if let messageBytesBaseAddress = messageBytes.baseAddress,
+               let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                let messageLength = CC_LONG(messageData.count)
+                CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+            }
+            return 0
+        }
+    }
+    return digestData
+}
+
+func generateHash(timestamp: String, privateKey: String, publicKey: String) -> String {
+    let combinedString = timestamp + privateKey + publicKey
+    let hashData = MD5(string: combinedString)
+    let hash = hashData.map { String(format: "%02hhx", $0) }.joined()
+    return hash
+}
 
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
