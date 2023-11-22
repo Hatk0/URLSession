408875b6a427950ed7ab8baeb2651e3071952b4d
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-22 21:08:02 +0300

    Add the necessary data to work with Marvel API, create a URLComponent link

--- URLSession.playground/Contents.swift
+++ URLSession.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Foundation
-import CryptoKit
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
 
 func MD5(string: String) -> Data {
     let messageData = string.data(using:.utf8)!
@@ -28,7 +30,7 @@ func generateHash(timestamp: String, privateKey: String, publicKey: String) -> S
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { 
-        print("Ссылка сформированна некорректно")
+        print("Ссылка сформирована некорректно")
         return
     }
     URLSession.shared.dataTask(with: url) { data, response, error in
@@ -50,6 +52,34 @@ func getData(urlRequest: String) {
     }.resume()
 }
 
+// Данные для работы с Marvel API и вызов метода генерации hash
+let timestamp = String(Date().timeIntervalSince1970)
+let publicKey = "6ee356d5267181fe73bba904f4c82975"
+let privateKey = "90bedb28b32e2fdc7be275e7ae2a5d496f671908"
+let hash = generateHash(timestamp: timestamp, privateKey: privateKey, publicKey: publicKey)
+
+// Вывод информации в консоль про ts и hash
+print("Timestamp: \(timestamp)")
+print("Hash: \(hash)")
+
+// URLComponents для Marvel API
+var urlComponents = URLComponents()
+urlComponents.scheme = "https"
+urlComponents.host = "gateway.marvel.com"
+urlComponents.path = "/v1/public/characters/231311/series"
+urlComponents.queryItems = [
+    URLQueryItem(name: "ts", value: timestamp),
+    URLQueryItem(name: "apikey", value: publicKey),
+    URLQueryItem(name: "hash", value: hash)
+]
+
+if let marvelURL = urlComponents.url {
+    getData(urlRequest: marvelURL.absoluteString)
+} else {
+    print("Невозможно создать адрес")
+}
+
+// Проверка на работу Meow API
 getData(urlRequest: "https://meowfacts.herokuapp.com/?lang=rus")
 
 // Проверка на работу некорректности ссылки
